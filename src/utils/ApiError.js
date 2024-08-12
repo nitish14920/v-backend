018f9c5cf08334517a1170a78f3ecac350ae7b5c
Nitish Kumar <nitish14920@gmail.com>
2024-08-12 17:37:22 +0530

    app.js Error Handle Api response utils

---
+++ src/utils/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Somthing went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.stack = stack;
+    this.errors = errors;
+    this.data = null;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
